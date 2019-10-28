2288aa2982aad6d39bb051a9bbae0e6a0d39c85c
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-10-28 11:28:46 +0700

    Initial Commit

---
+++ OrderEat-Customer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 28/10/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrderEat-CustomerTests/OrderEat_CustomerTests.swift
@@ -0,0 +1,34 @@
+//
+//  OrderEat_CustomerTests.swift
+//  OrderEat-CustomerTests
+//
+//  Created by Galih Asmarandaru on 28/10/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import XCTest
+@testable import OrderEat_Customer
+
+class OrderEat_CustomerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
