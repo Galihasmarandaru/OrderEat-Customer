b3097f92b32e468758aedd84692ebf34d9d795c6
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-10-29 18:42:04 +0700

    Custom Model and Segment Storyboard

---
+++ OrderEat-Customer/Model/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 29/10/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+struct Menu {
+    private var menuName: String;
+    private var detailMenu: String;
+    private var priceMenu: Int;
+    private var imageMenu: UIImage;
+}
---
+++ OrderEat-Customer/Model/Restaurant.swift
@@ -0,0 +1,33 @@
+//
+//  Restaurant.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 29/10/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+enum statusOperation {
+    case open, close
+}
+
+struct Restaurant {
+    private var restaurantName: String;
+    private var coordinatRestaurantL: CLLocation;
+    private var timeOpenRestaurant: UIDatePicker;
+    private var timeCloseRestaurant: UIDatePicker;
+    
+    var condition: String;
+    
+    mutating func statusOperationRestaurant(status: statusOperation) -> String {
+        switch status {
+        case .open:
+            condition = "Open until, \(timeCloseRestaurant)"
+        case .close:
+            condition = "Close until, \(timeOpenRestaurant)"
+        }
+        return condition
+    }
+}
