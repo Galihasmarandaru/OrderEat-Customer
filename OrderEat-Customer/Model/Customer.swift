441338cfe2c4bdba7be9cad8b38c9133b2434aa7
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-10-30 09:56:51 +0700

    Refactor Storyboard

---
+++ OrderEat-Customer/Model/Customer.swift
@@ -0,0 +1,19 @@
+//
+//  Customer.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 29/10/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+struct Customer {
+    private var customerName: String;
+    private var emailCustomer: String;
+    private var phoneNumberCustomer: String;
+    private var passwordCustomer: String;
+    
+    private var orderNumberCustomer: String;
+    private var pickupTimeCustomer: UIDatePicker;
+}
