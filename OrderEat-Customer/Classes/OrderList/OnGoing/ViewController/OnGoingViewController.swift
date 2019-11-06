1e2da0a19cd3a84c4652176da869a6909769536b
Malvin Santoso <malvinsantoso023@gmail.com>
2019-11-06 09:34:01 +0700

    OnGoing Segemnted Control DONE

--- OrderEat-Customer/Classes/OrderList/OnGoing/ViewController/OnGoingViewController.swift
+++ OrderEat-Customer/Classes/OrderList/OnGoing/ViewController/OnGoingViewController.swift
@@ -1,9 +1,48 @@
 //
-//  ViewControllerOrderList.swift
+//  OnGoingViewController.swift
 //  OrderEat-Customer
 //
-//  Created by Galih Asmarandaru on 31/10/19.
+//  Created by Malvin Santoso on 05/11/19.
 //  Copyright © 2019 Galih Asmarandaru. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OnGoingViewController: UIViewController {
+
+    @IBOutlet weak var historyButton: UIButton!
+    @IBOutlet weak var onGoingButton: UIButton!
+    @IBOutlet weak var onGoingUnderline: UIImageView!
+    @IBOutlet weak var historyUnderline: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        historyUnderline.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onGoingClicked(_ sender: Any) {
+        onGoingUnderline.isHidden = false
+        historyUnderline.isHidden = true
+        onGoingButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        historyButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0)
+    }
+    
+    @IBAction func historyClicked(_ sender: Any)
+    {
+        onGoingUnderline.isHidden = true
+        historyUnderline.isHidden = false
+        onGoingButton.titleLabel?.font = UIFont.systemFont(ofSize: 18.0)
+        historyButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
