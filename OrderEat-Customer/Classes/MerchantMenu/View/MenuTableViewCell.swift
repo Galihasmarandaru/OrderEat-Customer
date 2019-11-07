72b2d17a26f9ff1af2019c7866ec8fe34d56dedf
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-11-07 14:34:59 +0700

    update selected

--- OrderEat-Customer/Classes/MerchantMenu/View/MenuTableViewCell.swift
+++ OrderEat-Customer/Classes/MerchantMenu/View/MenuTableViewCell.swift
@@ -17,6 +17,31 @@ class MenuTableViewCell: UITableViewCell {
     
     @IBOutlet weak var addButton: UIButton!
     
+    @IBOutlet weak var minusTap: UIButton!
+    @IBOutlet weak var plusTap: UIButton!
+    @IBOutlet weak var qtyItem: UILabel!
+    
+    @IBOutlet weak var viewStepper: UIView!
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        sender.isHidden = !sender.isHidden
+        viewStepper.isHidden = false
+    }
+    
+    @IBAction func minusTap(_ sender: Any) {
+        if (data.qty == 0) {
+            viewStepper.isHidden = true
+            addButton.isHidden = false
+        } else {
+            data.qty -= 1
+        }
+    }
+    
+    @IBAction func plusTap(_ sender: Any) {
+        data.qty += 1
+    }
+    
+    
     var theData = AddDataMerchantMenu.getDataMenu()
     
     var data: Menu! {
@@ -25,9 +50,7 @@ class MenuTableViewCell: UITableViewCell {
             self.titleFood.text = data.nameMenu
             self.detailFood.text = data.detailMenu
             self.price.text = String("Rp \(data.priceMenu)")
-            
-//            self.addButton
+            self.qtyItem.text = String(data.qty)
         }
     }
-    
 }
