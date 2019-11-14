fedd83d7037226b099c4c0384657d9595c08b97a
galih asmarandaru <galih.asmarandaru@gmail.com>
2019-11-14 11:43:22 +0700

    coba

---
+++ OrderEat-Customer/Classes/CheckoutOrder/OrderSetTime/ViewModel/OrderDoneViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  OrderDoneViewModel.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 14/11/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import Foundation
---
+++ OrderEat-Customer/Classes/CheckoutOrder/OrderSetTime/ViewModel/OrderSetTimeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  OrderSetTimeViewModel.swift
+//  OrderEat-Customer
+//
+//  Created by Galih Asmarandaru on 14/11/19.
+//  Copyright © 2019 Galih Asmarandaru. All rights reserved.
+//
+
+import UIKit
+
+class OrderSetTimeViewModel: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
